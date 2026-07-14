1d40af66ba2558b7073c275a447a5c7dde1ed21f
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-14 19:33:57 +0530

    setters

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,5 +1,9 @@
 package org.example;
 
+import com.springcore.Student;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -9,5 +13,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+        Student student1= (Student) context.getBean("student1");
+        System.out.println(student1);
     }
 }
