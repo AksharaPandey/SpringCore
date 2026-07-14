7c9b626b11a28ddbb30fa62608c8e12cddb8b14d
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-14 20:35:50 +0530

    Beans for collections

---
+++ src/main/java/com/springcore/collections/Test.java
@@ -0,0 +1,8 @@
+package com.springcore.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/collections");
+}
