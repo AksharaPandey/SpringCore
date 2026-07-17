67c76b58842023497f0dba25f39c5501cb428705
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-17 15:55:26 +0530

    Lifecycle config using xml

---
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -0,0 +1,11 @@
+package com.springcore.lifecycle;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+        ApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
+        Samosa s1= (Samosa) context.getBean("s1");
+    }
+}
