a80f9a7e1fbb32619841c99b3a226823ca470749
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-20 15:13:55 +0530

    lifecycle

--- src/main/java/com/springcore/lifecycle/Test.java
+++ src/main/java/com/springcore/lifecycle/Test.java
@@ -1,14 +1,16 @@
 package com.springcore.lifecycle;
 
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-        AbstractApplicationContext context= new ClassPathXmlApplicationContext("com/springcore/lifecycle/lifecycleconfig.xml");
+        AbstractApplicationContext context= new ClassPathXmlApplicationContext("lifecycleconfig.xml");
         Samosa s1= (Samosa) context.getBean("s1");
         System.out.println(s1);
         context.registerShutdownHook();
+        System.out.println("++++++++++++++++++++++++++++++++");
+        Pepsi p1= (Pepsi) context.getBean("p1");
+        System.out.println(p1);
     }
 }
