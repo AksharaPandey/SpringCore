d766b51c2e0b2808601cf423d968d7624a09eb2f
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-24 17:07:15 +0530

    Need for SAL

--- src/main/java/com/springcore/collections/Test.java
+++ src/main/java/com/springcore/collections/Test.java
@@ -4,5 +4,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Test {
-    ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/collections");
+    public static void main(String[] args) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/collections");
+        Emp emp1= (Emp) context.getBean("emp1");
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getPhones());
+        System.out.println(emp1.getAddress());
+        System.out.println(emp1.getCourses());
+
+
+
+
+
+    }
+
 }
