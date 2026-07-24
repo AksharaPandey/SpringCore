9e2958e7f376b9341d6daf0d4b8f31c8dbb81afe
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-24 17:20:14 +0530

    Standalone collections list

---
+++ src/main/java/com/springcore/standalonecollection/Temp.java
@@ -0,0 +1,22 @@
+package com.springcore.standalonecollection;
+
+import java.util.List;
+
+public class Temp {
+    private List<String> friends;
+
+    public List<String> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(List<String> friends) {
+        this.friends = friends;
+    }
+
+    @Override
+    public String toString() {
+        return "Temp{" +
+                "friends=" + friends +
+                '}';
+    }
+}
---
+++ src/main/java/com/springcore/standalonecollection/Test.java
@@ -0,0 +1,4 @@
+package com.springcore.standalonecollection;
+
+public class Test {
+}
