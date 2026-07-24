5d473a725d5dcd26a64e172314cc7069aefdca75
akshara pandey <92318859+AksharaPandey@users.noreply.github.com>
2026-07-24 17:26:22 +0530

    Final commit

--- src/main/java/com/springcore/standalonecollection/Test.java
+++ src/main/java/com/springcore/standalonecollection/Test.java
@@ -1,4 +1,13 @@
 package com.springcore.standalonecollection;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
+    public static void main(String[] args, boolean person1) {
+        ApplicationContext context=new ClassPathXmlApplicationContext("com/springcore/standalonecollection/salconfig.xml");
+        context.getBean("person1",Temp.class);
+        System.out.println(person1);
+
+    }
 }
